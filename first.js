3a9734d511af454c124fdf80d5f1dec167c1cfde
aroomafatima <70141626@student.uol.edu.pk>
2024-12-11 13:41:47 +0500

    my comment

---
+++ first.js
@@ -0,0 +1,10 @@
+var food = "hot";
+if (food == "hot") {
+    console.log('Too hot')
+}
+else if (food == "cold"){ 
+    console.log('Too cold')
+}
+else{
+    console.log('just right')
+}
---
+++ firstlec.js
@@ -0,0 +1,15 @@
+//switch statement
+var place ='first';
+switch(place){
+    case 'first':
+        console.log('Gold');
+        break;
+        case 'second':
+            console.log('silver')
+        case 'thied':
+            console.log('Bronze')
+            break;
+            default:
+                console.log('no medal');
+
+}
